aad064df02a1179ec711ea30102da3a05b37d684
unknown <agangopa@DCODAPP030.aimco.com>
2016-12-27 04:03:14 -0500

    no message

---
+++ _master_git_checkout_nant_scripts.cmd
@@ -0,0 +1,7 @@
+@echo off
+set currdir=%cd%
+\\grvs37\git\bin\git init
+\\grvs37\git\bin\git remote add -f origin \\DCODAPP030\E$\Arijita\GIT Local Repository
+\\grvs37\git\bin\git pull origin master
+\\grvs37\git\bin\git reset --hard origin/master
+pause
